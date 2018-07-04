295d66132955a4fc20da14407e032ddf551457c7
Angel Daniel Zavala Higuera <angelz@bisoftcln.com.mx>
2018-07-03 18:18:24 -0600

    Se consulta la localización

---
+++ app.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+
+const argv = require('yargs')
+                .options({
+                    direction: {
+                        alias: 'd',
+                        desc: 'Permite capturar una dirección',
+                        demand: true
+                    }
+                })
+                .argv;
+
+let encodeUrl = encodeURI(argv.direction);
+
+axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ encodeUrl }&key=AIzaSyDhFt0ujT-gFDDMZnG8BxyKLK3e4zCDmyg`)
+    .then(resp => {
+        let location  = resp.data.results[0].geometry.location;
+        let lat = location.lat;
+        let lng = location.lng;
+
+        console.log(lat);
+        console.log(lng);
+    })
+    .catch();
